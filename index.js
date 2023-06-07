c183897f30e68929978801b69273aac84ed9eb11
iamitpal <amitpalinfo@yahoo.in>
2023-06-07 16:46:37 +0530

    added Cors

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-import userRoutes from "./routes/users.js";
-import authRoutes from "./routes/auths.js";
-import tweetRoutes from "./routes/tweets.js";
+import userRoutes from "./routes/users.js"
+import authRoutes from "./routes/auths.js"
+import tweetRoutes from "./routes/tweets.js"
 
-const app = express();
-dotenv.config();
+const app = express()
+app.use(cors())
+dotenv.config()
 
 const connect = () => {
-  mongoose.set("strictQuery", false);
+  mongoose.set("strictQuery", false)
   mongoose
     .connect(process.env.MONGO)
     .then(() => {
-      console.log("connect to mongodb database");
+      console.log("connect to mongodb database")
     })
     .catch((err) => {
-      throw err;
-    });
-};
+      throw err
+    })
+}
 
-app.use(cookieParser());
-app.use(express.json());
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/tweets", tweetRoutes);
+app.use(cookieParser())
+app.use(express.json())
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/tweets", tweetRoutes)
 
 app.listen(8000, () => {
-  connect();
-  console.log("Listening to port 8000");
-});
+  connect()
+  console.log("Listening to port 8000")
+})
